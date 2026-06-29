a44f61381df23ca04526ec63e6fb434efb6a8a50
DingJun1028 <dingjunhong1028@gmail.com>
2026-06-29 19:21:12 +0800

    fix: update ESLint config for Next.js compatibility

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,9 +1,14 @@
 module.exports = {
-  extends: ['next/eslint-recommended', 'next/core-web-vitals-16'],
+  extends: ['plugin:@next/next/recommended'],
   rules: {
     '@next/next/no-img-element': 'off',
     'react/no-unescaped-entities': 'off',
     'react/jsx-no-target-blank': 'off',
   },
   ignorePatterns: ['node_modules/', '.next/', 'out/', '*.cjs', '*.mjs'],
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
 };
